03f3ddae9f8021bbce3c4ae7eb897ddedff116ec
Azizbek Rustamov <rystamov2233@gmail.com>
2024-06-22 14:52:09 +0500

    object

---
+++ index.js
@@ -0,0 +1,15 @@
+// let azizarr = ["Aziz", 15 , "tinchlik"];
+
+// console.log(`${azizarr[0]} ${azizarr[1]} da ${azizarr[2]} da turadi`);
+
+let ism = prompt("ismingizni kiriting");
+let id = Number(prompt("id kiriting"));
+let password = Number(prompt("password kiriting"));
+
+let azizobj = {
+    name: ism,
+    id: id,
+    password: password
+};
+
+console.log(azizobj);
